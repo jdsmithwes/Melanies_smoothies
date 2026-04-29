456e2747f6cef7a04ec32c45509ef82ba30c7057
jdsmithwes <53444854+jdsmithwes@users.noreply.github.com>
2026-04-29 18:57:26 +0000

    added api call to for loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,9 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(smoothiefroot_response.json(), use_container_width= True)
+
 
 
     time_to_insert = st.button("Submit Order")
@@ -44,6 +47,4 @@ if ingredients_list:
 
     #New section to display smoothiefroot nutrition information
 
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    st.text(smoothiefroot_response.json())
-    sf_df = st.dataframe(smoothiefroot_response.json(), use_container_width= True)
+    
