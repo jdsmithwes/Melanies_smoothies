10cb73de65147646d9ac4079a40c5c082b1b5f1b
jdsmithwes <53444854+jdsmithwes@users.noreply.github.com>
2026-04-29 20:30:18 +0000

    parameter deprecation warning adjustment for use_container_width

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ if ingredients_list:
     st.stop()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width= True)
+st.dataframe(data=my_dataframe, width='stretch')
 st.stop()
 
     
